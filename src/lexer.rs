0047fa50d2f12bdb2187c4f84db43ad29f6f9ffd
abim91 <53064345+abim91@users.noreply.github.com>
2025-10-31 19:08:35 -0400

    fixed read_hash_keyword

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,11 +1,11 @@
 pub trait LexicalAnalyzer {
-    fn get_char(&mut self) -> char; 
-    fn add_char(&mut self, c: char); 
-    fn lookup(&self, s: &str) -> bool; 
+    fn get_char(&mut self) -> char;
+    fn add_char(&mut self, c: char);
+    fn lookup(&self, s: &str) -> bool;
 }
 
 pub struct LolLexer {
-    input: Vec<char>, 
+    input: Vec<char>,
     index: usize,
     pub line: usize,
     pub col: usize,
@@ -23,7 +23,7 @@ impl LolLexer {
             line: 1,
             col: 0,
             potential_token: String::new(),
-            
+
             hash_keywords: [
                 "#HAI",
                 "#KTHXBYE",
@@ -47,7 +47,8 @@ impl LolLexer {
     //function to return the next valid token
     pub fn next_token(&mut self) -> String {
         self.skip_ws();
-        match self.peek() {//pattern matching to handle tokens that start with # and everything else
+        match self.peek() {
+            //pattern matching to handle tokens that start with # and everything else
             None => "EOF".to_string(),
 
             Some('#') => self.read_hash_keyword(),
@@ -57,37 +58,35 @@ impl LolLexer {
                 let up = w.to_ascii_uppercase();
                 if self.plain_keywords.contains(&up.as_str()) {
                     return up;
-                }else{
+                } else {
                     return format!("TEXT({})", w);
                 }
-
             }
         }
     }
 
-    //return next char 
+    //return next char
     fn peek(&self) -> Option<char> {
         if self.index < self.input.len() {
-            return Some(self.input[self.index])
+            return Some(self.input[self.index]);
         } else {
-            return None
+            return None;
         }
     }
-
 
     fn advance(&mut self) -> Option<char> {
         let c = self.peek();
 
         if c != None {
             self.index += 1;
-            if c == None {
+            if c == Some('\n') {
                 self.line += 1;
                 self.col = 0;
             } else {
                 self.col += 1;
             }
         }
-        return c
+        return c;
     }
 
     //a helper function to skip white spaces between tokens
@@ -104,51 +103,60 @@ impl LolLexer {
 
     //returns the next token
     fn get_token(&mut self) -> String {
-        
         self.potential_token.clear();
         while let Some(c) = self.peek() {
             if c.is_whitespace() || c == '#' {
                 break;
             }
-            let ch = self.get_char(); 
-            self.add_char(ch); 
+            let ch = self.get_char();
+            self.add_char(ch);
         }
         self.potential_token.clone()
     }
 
     fn read_hash_keyword(&mut self) -> String {
-        
         self.advance();
-        let first_token = self.get_token();
+        let first_token = self.get_token().to_ascii_uppercase();
 
         // some tokens such as "#I HAZ" have two parts, so we need to check for them
         let save_i = self.index;
         let save_line = self.line;
         let save_col = self.col;
-        self.skip_ws();
-        let second_token = self.get_token();
 
-        let two = if second_token.is_empty() {
-            format!("#{}", first_token).to_ascii_uppercase()
-        } else {
-            format!("#{} {}", first_token, second_token).to_ascii_uppercase()
+        if first_token.is_empty() { self.error("Expected keyword after '#'"); }
+
+        self.skip_ws();
+
+        let second_token = self.get_token().to_ascii_uppercase();
+        let mut potentional_FT = format!("#{} {}", first_token,second_token);
+        let complete_token = if self.lookup(&potentional_FT){
+            potentional_FT
+        }else {
+            format!("#{}",first_token)
         };
 
-    
-        if self.lookup(&two) {
-            return two.trim_start_matches('#').to_string();
+        if first_token == complete_token{
+             self.index = save_i;
+             self.line = save_line;
+             self.col = save_col;
         }
 
-        self.index = save_i;
-        self.line = save_line;
-        self.col = save_col;
+        if !self.lookup(&complete_token){
+            self.error(&format!("Not a valid and/or complete token '{}'", complete_token));
+        }
 
-        let one = format!("#{}", first_token).to_ascii_uppercase();
-        one.trim_start_matches('#').to_string()
+        return complete_token;
+
+    
     }
 
-    
-    
+    fn error(&self, msg: &str) {
+        eprintln!(
+            "Lexical error at line {}, col {}: {}",
+            self.line, self.col, msg
+        );
+        std::process::exit(1);
+    }
 }
 
 //implementation of trait functions
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 		#MAEK HEAD
 			#GIMMEH TITLE The Simpsons #MKAY
 		#OIC
-		#MAEK PARAGRAF
+		#I HAZ
 			The Simpsons! #GIMMEH NEWLINE
 			#GIMMEH SOUNDZ 
                 http://www.televisiontunes.com/themesongs/The%20Simpsons.mp3 
@@ -30,11 +30,27 @@ fn main() {
 	#KTHXBYE
 ";
 
+    let src2 = "#HAI
+#OBTW This test case assess that your compiler resolves a simple variable. #TLDR
+#MAEK HEAD 
+	#GIMMEH TITLE Test 6 #MKAY
+#OIC
+
+#MAEK PARAGRAF
+	#I HAZ answer #IT IZ 42 #MKAY
+	#GIMMEH ITALICS What is the meaning of life. #MKAY 
+	The meaning of #GIMMEH BOLD life #MKAY is #LEMME SEE answer #MKAY
+#OIC
+#KTHXBYE
+";
+
     let mut c = LolLexer::new(src);
 
     loop {
         let t = c.next_token();
         println!("{t}");
-        if t == "EOF" { break; }
+        if t == "EOF" {
+            break;
+        }
     }
 }
